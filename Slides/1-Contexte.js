f24e1139a55f6f57f1cbd52ef6c3a42707dfa067
Quentin WENDLING <quentin.wendling2@etu.unistra.fr>
2023-11-27 08:20:48 +0000

    Update 4 files

--- Slides/1-Contexte.js
+++ Slides/1-Contexte.js
@@ -1,14 +1,11 @@
-const xhttp = new XMLHttpRequest();
-let text;
 xhttp.onreadystatechange = function () {
   if (this.readyState === 4) {
     if (this.status === 200) {
-      text = this.responseText;
+      app.component("Slide_contexte", {
+        template: this.responseText,
+      });
     }
   }
 };
 xhttp.open("GET", "./Slides/1-Contexte.html", false);
 xhttp.send();
-app.component("Slide_contexte", {
-  template: text,
-});
